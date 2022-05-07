7a0aebf6d536b8b9a8c97cf4ac20c9fdb1c6cb67
lukaszkukla <lukaszkukla@gmail.com>
2022-05-07 18:37:12 +0000

    filter method

--- filter.js
+++ filter.js
@@ -21,9 +21,16 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(p => p.name === 'Paul');
+console.log(paul);
+
+
 
 // Complex Filtering
-const students = [
+const studentss = [
   {
     id: 1,
     name: 'Mark',
@@ -55,3 +62,39 @@ const students = [
     ]
   },
 ];
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+const candidates = studentss.filter(hasStrongSkills);
+const candNames = candidates.map(p => p.name)
+console.log(candNames);
+
+// code challenge
+let students = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 84, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 67, english: 87, art: 95},
+  }
+];
+
+const topMaths = students.filter(student => student.results.maths >=90);
+console.log(topMaths);
